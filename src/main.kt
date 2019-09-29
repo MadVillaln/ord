77c5a136449f030e1f03f107de9304a0a63aa574
MadVillaln <dmitriy.vorotilov@gmail.com>
2019-09-29 23:19:36 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,6 @@
+fun main(args:Array<String>){
+    val sq=Array(7){i -> i*i }
+    print(sq)
+    //print(shorten(""))
+}
+
---
+++ src/shorten.kt
@@ -0,0 +1,4 @@
+fun shorten(name:String?)=
+    if (true==name?.isNotEmpty()) "${name[0]}."
+    else "fuck"
+
